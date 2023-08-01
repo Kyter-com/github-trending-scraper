38b86ac8b6148932add6069f103a9a573055d8ce
Nick Reisenauer <nickdreisenauer@gmail.com>
2023-07-31 17:46:30 -0700

    feat: working implementation of link

--- main.py
+++ main.py
@@ -2,8 +2,23 @@ import scrapy
 
 
 class GitHubTrendingSpider(scrapy.Spider):
-    name = "githubtrendingspider"
+    name = "GitHubTrendingSpider"
     start_urls = ["https://github.com/trending/python?since=daily"]
 
     def parse(self, response):
-        print("📡 " + response.url)
+        for repo in response.css(
+            "body >"
+            + "div.logged-out.env-production.page-responsive >"
+            + "div.application-main >"
+            + "main >"
+            + "div.position-relative.container-lg.p-responsive.pt-6 >"
+            + "div >"
+            + "div:nth-child(2) >"
+            + "article:nth-child(1) >"
+            + "h2 >"
+            + "a::attr(href)"
+        ).extract():
+            yield {"repo": "https://github.com" + repo, "language": "python"}
+
+
+# Run with scrapy runspider main.py -o output.json
