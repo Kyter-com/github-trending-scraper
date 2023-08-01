35641f90198f15a2589d1c1b03b7d76b66389cc4
Nick Reisenauer <38516489+NickReisenauer@users.noreply.github.com>
2023-08-01 19:04:16 +0000

    feat: run scraper inside file

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
+"""Scrapy library import"""
 import scrapy
+from scrapy.crawler import CrawlerProcess
 
 
-class GitHubTrendingSpider(scrapy.Spider):
+SHARED_CSS_SELECTOR = (
+    "body >"
+    + "div.logged-out.env-production.page-responsive >"
+    + "div.application-main >"
+    + "main >"
+    + "div.position-relative.container-lg.p-responsive.pt-6 >"
+    + "div >"
+    + "div:nth-child(2) >"
+    + "article:nth-child(1) >"
+    + "h2 >"
+    + "a::attr(href)"
+)
+
+
+class GitHubTrendingPythonSpider(scrapy.Spider):
+    """Spider for daily trending Python repositories"""
+
     name = "GitHubTrendingSpider"
     start_urls = ["https://github.com/trending/python?since=daily"]
 
     def parse(self, response):
-        for repo in response.css(
-            "body >"
-            + "div.logged-out.env-production.page-responsive >"
-            + "div.application-main >"
-            + "main >"
-            + "div.position-relative.container-lg.p-responsive.pt-6 >"
-            + "div >"
-            + "div:nth-child(2) >"
-            + "article:nth-child(1) >"
-            + "h2 >"
-            + "a::attr(href)"
-        ).extract():
+        for repo in response.css(SHARED_CSS_SELECTOR).extract():
             yield {"repo": "https://github.com" + repo, "language": "python"}
 
 
-# Run with scrapy runspider main.py -o output.json
+process = CrawlerProcess()
+process.crawl(GitHubTrendingPythonSpider)
+process.start()
